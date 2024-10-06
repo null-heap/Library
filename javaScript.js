5bcd39f153713e8da121e4ebf33204226477b21e
null-heap <142169162+null-heap@users.noreply.github.com>
2024-10-06 16:36:22 +0700

    feat: added card display function through js

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,7 @@
 const myLibrary = [];
+const myLibraryElements = [];
+
+const bookCards = document.querySelector("#bookCards");
 
 
 // i have two options to help me keep track of the books id, use count or rely on myLibrary.length.
@@ -17,11 +20,60 @@ Book.prototype.info = function(){
 };
 
 Book.prototype.display = function(){
+        // now the html elements creation
+        let divCard = document.createElement("div");
+        divCard.className = "card";
+        //title element
+        divCard.appendChild(addToCard("title","Title: ", this.title));
+        //author element
+        divCard.appendChild(addToCard("author","Author: ", this.author));
+        //pages element
+        divCard.appendChild(addToCard("pages","Pages: ", this.pages));
+        //read button
+        let div = document.createElement("div");
+        div.className = "read";
+    
+        let wordSpan = document.createElement('span');
+        wordSpan.textContent = "Read: ";
+        wordSpan.className = "cardStrong";
+    
+        let valueSpan = document.createElement('button');
+        valueSpan.textContent = this.read;
+        valueSpan.className = "readButton";
+    
+        div.appendChild(wordSpan);
+        div.appendChild(valueSpan);
+    
+        divCard.appendChild(div);
+        //id element
+        divCard.appendChild(addToCard("id", "Book Id: ", this.id))
+    
+        myLibraryElements.push(divCard);
+        bookCards.appendChild(divCard);
 
 };
 
+function addToCard(name, word, value){
+    let div = document.createElement("div");
+    div.className = name;
+
+    let wordSpan = document.createElement('span');
+    wordSpan.textContent = word;
+    wordSpan.className = "cardStrong";
+
+    let valueSpan = document.createElement('span');
+    valueSpan.textContent = value;
+    valueSpan.className = "value";
+
+    div.appendChild(wordSpan);
+    div.appendChild(valueSpan);
+    return div;
+}
+
+
 function addBookToLibrary(title, author, pages, read){
     myLibrary.push(new Book(title, author, pages, read, myLibrary.length));
+    myLibrary[myLibrary.length - 1].display();
 }
 
 function removeFromLibrary(id){
@@ -32,3 +84,5 @@ function removeFromLibrary(id){
     // Book.count --;
     return removed;
 }
+
+// the read button funcitonality
